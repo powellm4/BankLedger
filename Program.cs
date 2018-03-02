66cf661b6189028e51f62858aa8d4d8256581839
Marshall Powell <marshall.powell@nwea.org>
2018-03-02 09:04:36 -0800

    added User class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,39 @@
-﻿using System;
+﻿/*
+You have been tasked with writing the world’s greatest banking ledger. Please code a solution
+that can perform the following workflows through a console application (accessed via the 
+command line):
+
+-Create a new account
+-Login
+-Record a deposit
+-Record a withdrawal
+-Check balance
+-See transaction history
+-Log out
+
+For additional credit, you may implement this through a web page. They don’t have to run at 
+the same time, but if you would like to do that, feel free.
+
+C# is preferred but not required. Use whatever frameworks/libraries you wish, and just make
+sure they are included or available via via NuGet/npm/etc. Please use a temporary memory store
+(local cache) instead of creating an actual database, and don't spend much time on the UI
+(unless you love doing that).
+
+
+
+
+todo:
+    create user class
+    store users in list
+    enable user creation via command line
+
+ */
+
+
+
+
+
+using System;
 
 namespace BankLedger
 {
@@ -7,6 +42,7 @@ namespace BankLedger
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            User TestUser = new User("user1","password1");
         }
     }
 }
---
+++ User.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace BankLedger
+{
+    class User
+    {
+        private string Username;
+        private string Password;
+        private string Name;
+        private string Balance;
+
+
+        public User (string userName, string password){
+            this.Username = userName;
+            this.Password = password;
+            Console.WriteLine("Created new User with Username:{0}, Password: {1}", this.Username,this.Password);
+        }
+            
+
+
+
+
+    }
+}
